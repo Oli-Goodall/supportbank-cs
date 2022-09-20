de104c9713de769c2a8e36d0bbd5719998fdb3ed
Leo Ng <leo.ng@softwire.com>
2022-09-20 13:36:47 +0100

    trying to do logging

--- CsvReader.cs
+++ CsvReader.cs
@@ -4,12 +4,29 @@ namespace SupportBank
 {
     public static class CsvReader
     {
+        
         public static Company ReadFromFile(string path)
         {
+            
             using (var reader = new StreamReader(path))
             using (var csvReader = new CsvHelper.CsvReader(reader, CultureInfo.InvariantCulture))
             {
-                List<Transaction> transactions = csvReader.GetRecords<Transaction>().ToList();
+                csvReader.Read();
+                csvReader.ReadHeader();
+                while(csvReader.Read())
+                {
+                    try
+                    {
+                        Transaction transaction = csvReader.GetRecord<Transaction>();
+                        transactions.Add(transaction);
+                    }
+                    catch (CsvHelper.TypeConversion.TypeConverterException ex)
+                    {
+                        Logger.Error(ex);
+                        
+                    }
+                }
+                List<Transaction> transactions = csvReader.GetRecord<Transaction>().ToList();
                 return new Company(transactions);
             }
         }
--- Program.cs
+++ Program.cs
@@ -1,13 +1,30 @@
-﻿namespace SupportBank
+﻿using NLog;
+using NLog.Config;
+using NLog.Targets;
+
+
+
+namespace SupportBank
 {
     class Program
     {
+        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
         static void Main(string[] args)
         {
-            // for csv
+        
+            var config = new LoggingConfiguration();
+            var target = new FileTarget { FileName = @"C:\Users\LeoNg\Work\supportbank\Logs\SupportBank.log", Layout = @"${longdate} ${level} - ${logger}: ${message}" };
+            config.AddTarget("File Logger", target);
+            config.LoggingRules.Add(new LoggingRule("*", LogLevel.Debug, target));
+            LogManager.Configuration = config;
+
             Company TechSwitch = CsvReader.ReadFromFile("Transactions2014.csv");
             PrintOut.AskUserWhatNeedToDo(TechSwitch);
-            //for json
+
+            Logger.Debug("Hello");
+    
+           
+            
         }
     }
 }
