3ecf7421dd26d1b37610a310ceb9c6008737808d
Tim Leach <timleach635@gmail.com>
2022-09-20 11:01:44 +0100

    Refactors
    Bring out the CSV reading functionality into a separate file, do some
    renaming, and removing a bunch of unnecessary imports

--- CsvData.cs
+++ CsvData.cs
@@ -1,11 +1,3 @@
-using System;
-using CsvHelper;
-using System.IO;
-using System.Globalization;
-using System.Linq;
-using CsvHelper.Configuration;
-using CsvHelper.Configuration.Attributes;
-
 namespace SupportBank
 {
     public class CsvData
@@ -13,14 +5,10 @@ namespace SupportBank
         public static HashSet<string> GetNameList(List<Transaction> transactions)
         {
             HashSet<string> fromNameHSet = new HashSet<string>();
-            foreach (var item in transactions)
-            {
-                fromNameHSet.Add(item.FromName);
-            }
-
             HashSet<string> toNameHSet = new HashSet<string>();
             foreach (var item in transactions)
             {
+                fromNameHSet.Add(item.FromName);
                 toNameHSet.Add(item.ToName);
             }
 
--- Person.cs
+++ Person.cs
@@ -1,11 +1,3 @@
-using System;
-using CsvHelper;
-using System.IO;
-using System.Globalization;
-using System.Linq;
-using CsvHelper.Configuration;
-using CsvHelper.Configuration.Attributes;
-
 namespace SupportBank
 {
     public class Person
